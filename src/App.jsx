6b93078e0830a6372caa96da1cf7b630ae0d4cc8
Brandon Saguil <bsaguil@udallas.edu>
2025-07-16 10:59:20 -0500

    Moved Sidebar to App.jsx and rendered at app level to be persistent.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
+import React, { useState } from "react";
 import Home from "./pages/Home.jsx";
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
+import Sidebar from "./components/Sidebar.jsx";
 import "./index.css";
 
 function App() {
+  const [showSidebar, setShowSidebar] = useState(false);
+
   return (
     <div className="main-page-container">
-      <Header></Header>
-      <Home></Home>
-      <Footer></Footer>
+      <div className="app-layout flex">
+        {/* Persistent Sidebar */}
+        <div className="sidebar-container">
+          <Sidebar showSidebar={showSidebar} setShowSidebar={setShowSidebar} />
+        </div>
+        
+        {/* Main Content Area */}
+        <div className="main-content-area flex-1">
+          <Header />
+          <Home />
+          <Footer />
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,13 @@
 import React from "react";
 import Card from "../components/Card";
 import Events from "../components/Events";
-import Sidebar from "../components/Sidebar";
-import { useState } from "react";
 import WeeklyEventCard from "../components/WeeklyEventCard";
 import AccordionContainer from "../components/AccordionContainer";
 import GradeSnapshot from "../components/GradeSnapshot";
 
 const Home = () => {
-  const [showSidebar, setShowSidebar] = useState(false);
-
   return (
     <div id="home-container" className="flex row">
-      <div className="sidebar-container">
-        <Sidebar showSidebar={showSidebar} setShowSidebar={setShowSidebar} />
-      </div>
       <div
         id="card-container"
         className="flex row flex-wrap gap-4 items-center justify-between mx-auto p-4"
@@ -29,7 +22,6 @@ const Home = () => {
       <div id="event-container" className="">
         <Events /> {/* Events  and To do */}
       </div>
-      {/* </div> */}
     </div>
   );
 };
